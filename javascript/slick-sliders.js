9fa1c520da8d839d1a28411cc37dfed14330daa8
axCaffiend <axfulcher@outlook.com>
2024-01-15 02:31:47 +0000

    Styled testimonials on home page, as a slick slider.

---
+++ javascript/slick-sliders.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.testimonials').slick({
+    infinite: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+
+  alert("slick-slider running")
+});
