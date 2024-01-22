c1d7038a2b6e9de51890135b25ca882ed124f1b1
axCaffiend <axfulcher@outlook.com>
2024-01-22 21:48:41 +0000

    About-page - added info-card--quote variant, and only-child-of-type rule to resize larger if only one card in info-card-block. Styled image-gallery--slider and added to javascript slick-sliders.js. Styled video sizing in Zumbi about section as page-section_video.

--- javascript/slick-sliders.js
+++ javascript/slick-sliders.js
@@ -6,5 +6,12 @@ $(document).ready(function(){
     autoplaySpeed: 3000,
   });
 
+  $('.image-gallery--slider').slick({
+    infinite: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+
   // alert("slick-slider running")
 });
